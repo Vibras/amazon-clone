5df6871f30a9cef18b9e8bb321668a24db110918
Ricky <rheredia420@gmail.com>
2021-09-10 02:04:34 -0600

    Added ability to sign in/out of your account.

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home.js";
 import Header from "./Header.js";
 import Login from './Login.js';
 import Checkout from "./Checkout.js";
-// import { auth } from "./firebase";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider.js";
+
 
 function App() {
-  
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     // BEM
